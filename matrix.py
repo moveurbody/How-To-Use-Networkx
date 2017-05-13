5caa51680c81a03f5087d18fc6a28866b2b2e352
moveurbody <a_gangan@hotmal.com>
2017-05-14 01:27:59 +0800

    畫出圖形

--- matrix.py
+++ matrix.py
@@ -16,8 +16,8 @@ matrix = np.asmatrix(csv)
 # 轉致matrix，就是one_mode
 matrix_t = matrix.T
 
-print(matrix)
-print(matrix_t)
+# print(matrix)
+# print(matrix_t)
 # 算出matrix乘績
 one_mode = np.dot(matrix,matrix_t)
 print(one_mode)
@@ -25,19 +25,20 @@ print(one_mode)
 print(one_mode[1,1])
 
 # 取得多少node
-print(len(matrix))
+matrix_len = len(matrix)
+print(matrix_len)
 
 G = nx.from_numpy_matrix(one_mode)
 pos=nx.spring_layout(G)
 
 # 增加node
 nodelist = []
-for node in range(0,len(matrix)):
+for node in range(0,matrix_len):
     nodelist.append(node)
 
 # 增加node的名稱
 labels = {}
-for label in range(0,len(matrix)):
+for label in range(0,matrix_len):
     labels[label] = "%s" %(label)
 
 print(nx.info(G))
@@ -50,18 +51,19 @@ nx.draw_networkx_labels(G,pos,labels=labels,font_size=8)
 # 將edges畫出來
 nx.draw_networkx_edges(G,pos,width=1.0,alpha=0.5,edge_color='b')
 
-# 將中心度列出來
-print('\nbetweenness')
-print(nx.betweenness_centrality(G))
-# 將G的資料列出來
-print('\ninfo G')
-print(nx.info(G))
-# 將各edges列出來
-print('\nedge G')
-print(nx.edges(G))
+# # 將中心度列出來
+# print('\nbetweenness')
+# print(nx.betweenness_centrality(G))
+# # 將G的資料列出來
+# print('\ninfo G')
+# print(nx.info(G))
+# # 將各edges列出來
+# print('\nedge G')
+# print(nx.edges(G))
+
 # 將matplotlib的座標關閉
 plt.axis('off')
 # 存檔
-plt.savefig("labels_and_colors.png")
+plt.savefig("Matrix_Output.png")
 
 plt.show()
